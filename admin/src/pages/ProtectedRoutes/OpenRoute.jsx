26d54f0ea0fcd5eaad8ff7a5fb789b3637b7fff2
mikeadesina <michaeladeshina015@gmail.com>
2024-02-12 11:05:20 +0100

    remove unused folders

--- admin/src/pages/ProtectedRoutes/OpenRoute.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import {Navigate} from "react-router-dom"
-
-export const OpenRoutes = ({children}) => {
-    const getTokenFromLocalStorage = JSON.parse(localStorage.getItem("user"))
-    return getTokenFromLocalStorage?.token == undefined ? children : (<Navigate to="/admin" replace={true}/>)
-}
